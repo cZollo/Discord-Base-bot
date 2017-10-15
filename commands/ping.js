e1f5e9684f076d9c40f0cd5df1391f76bc00fc9e
cZollo <26638439+cZollo@users.noreply.github.com>
2017-10-15 18:14:38 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+    message.channel.send("Pong, that took " + new Date().getTime() - message.createdTimestamp + " ms");
 }
