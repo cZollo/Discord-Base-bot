d1aca5e2d6d8bde5573520ca61be24114cdd4181
cZollo <26638439+cZollo@users.noreply.github.com>
2017-10-15 19:46:12 +0100

    Ban command created, hopefully right!

---
+++ commands/ban.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, [mention, ...reason]) => {
+  let modRole = message.guild.roles.find("name", "Moderator");
+    
+  if(message.member.roles.has(modRole.id)) { 
+
+	  
+	  if (!message.mentions.users.first()) {
+		  return message.reply("You must mention a user to ban them.")
+	  }
+      
+	  let banMember = message.guild.member(message.mentions.users.first())
+      message.guild.member(banMember).kick();
+      message.channel.send("User was Banned.");
+    } else {
+      return message.reply("You must have the role `Moderator` to use this command");
+    }
+};
